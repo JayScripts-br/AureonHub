d79d0c9587c2c838d551198ba7881a681255e59d
JayScripts-br <jotalkzee@gmail.com>
2025-02-01 14:39:26 -0300

    Update and rename MainScript to MainScript.lua

---
+++ MainScript.lua
@@ -0,0 +1,4 @@
+if game.PlaceId == 16732694052 then
+ loadstring(game:HttpGet("https://raw.githubusercontent.com/JayScripts-br/AureonHub/refs/heads/main/gamescripts/Fisch.txt"))()
+ print("Fisch Script Loaded")
+end

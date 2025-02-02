9663e824b200774e80a7f36a648ef88b00571348
JayScripts-br <jotalkzee@gmail.com>
2025-02-02 18:30:29 -0300

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,4 +1,4 @@
 if game.PlaceId == 16732694052 then
- loadstring(game:HttpGet("https://raw.githubusercontent.com/JayScripts-br/AureonHub/refs/heads/main/gamescripts/Fisch"))()
+ loadstring(game:HttpGet("https://raw.githubusercontent.com/JayScripts-br/AureonHub/refs/heads/main/gamescripts/fisch/Fisch"))()
  print("Fisch Script Loaded")
 end

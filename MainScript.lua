672012c6bf2cdc7d71c9bf8f66d8ddd36444efeb
JayScripts-br <jotalkzee@gmail.com>
2025-02-03 10:35:56 -0300

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -11,9 +11,11 @@ end
 
 if game.PlaceId == 16732694052 then
  if result and code == "BR" then
- Notify("Aureon Hub", "Detectamos que você está no Brasil, então, a versão brasileira foi executada.", 7)
+ Notify("Aureon Hub", "Detectamos que você está no Brasil.", 7)
  loadstring(game:HttpGet("https://raw.githubusercontent.com/JayScripts-br/AureonHub/refs/heads/main/Scripts/FischBR.lua"))()
+ Notify("Aureon Hub", "Hub carregado com sucesso!", 7)
  else
  loadstring(game:HttpGet("https://raw.githubusercontent.com/JayScripts-br/AureonHub/refs/heads/main/Scripts/Fisch.lua"))()
+ Notify("Aureon Hub", "Hub loaded successfully!", 7)
  end
 end

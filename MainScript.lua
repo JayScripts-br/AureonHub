aaa9dae31e998b6070a6ffaddf3cc74f9099809d
JayScripts-br <jotalkzee@gmail.com>
2025-02-02 21:30:24 -0300

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,4 +1,11 @@
+local Player = game.Players.LocalPlayer
+local LocalizationService = game:GetService("LocalizationService")
+local result, code = pcall(LocalizationService.GetCountryRegionForPlayerAsync, LocalizationService, Player)
+
 if game.PlaceId == 16732694052 then
+ if result and code == "BR" then
+ loadstring(game:HttpGet("https://raw.githubusercontent.com/JayScripts-br/AureonHub/refs/heads/main/Scripts/FischBR.lua"))()
+ else
  loadstring(game:HttpGet("https://raw.githubusercontent.com/JayScripts-br/AureonHub/refs/heads/main/Scripts/Fisch.lua"))()
- print("Fisch Script Loaded")
+ end
 end
